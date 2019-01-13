6429c227a377edbeb6ab7a6ff9129852d5c1c8f6
J2525 <blue.east1028@gmail.com>
2019-01-13 17:02:55 +0900

    modify myapp.rb

--- myapp.rb
+++ myapp.rb
@@ -2,8 +2,22 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
-get "/ping" do
-  content_type :json
-  data = { "result": "pong" }
-  data.to_json
+ACCESS_TOKEN = ENV["ACCESS_TOKEN"]
+
+post "/ping" do
+  data = JSON.parse request.body.read
+  text = data["events"][0]["message"]["text"]
+  reply_token = data["events"][0]["replyToken"]
+
+  if text == "ping"
+    cmd = <<-HERE
+    curl -X POST \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer #{ACCESS_TOKEN}" \
+    -d '{"replyToken":"#{reply_token}","messages":[{"type":"text","text":"pong"}]}' \
+    https://api.line.me/v2/bot/message/reply
+    HERE
+    system(cmd)
+    p "daijobu"
+  end
 end
